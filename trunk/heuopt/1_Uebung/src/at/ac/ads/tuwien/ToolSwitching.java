37ce2c785da51b3cd14537f5452c52531d6d71f7
reiter.j <reiter.j@10957a6e-c882-11de-8672-e51b9359e43c>
2009-11-03 17:16:24 +0000

    new project

---
+++ trunk/heuopt/1_Uebung/src/at/ac/ads/tuwien/ToolSwitching.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package at.ac.ads.tuwien;
+
+/**
+ * @author Johannes REITER
+ *
+ */
+public class ToolSwitching {
+	
+	private ToolSwitching() {
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		new ToolSwitching();
+
+	}
+
+}
