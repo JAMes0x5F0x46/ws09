5b37e9871a2604909aad32b18b7f7157a87daceb
reiter.j <reiter.j@10957a6e-c882-11de-8672-e51b9359e43c>
2009-11-05 16:46:45 +0000

    importer

--- trunk/heuopt/1_Uebung/src/at/ac/ads/tuwien/ToolSwitching.java
+++ trunk/heuopt/1_Uebung/src/at/ac/ads/tuwien/ToolSwitching.java
@@ -1,6 +1,10 @@
 
 package at.ac.ads.tuwien;
 
+import java.io.File;
+import java.util.Map;
+import java.util.Set;
+
 
 /**
  * @author Johannes REITER
@@ -8,9 +12,15 @@ package at.ac.ads.tuwien;
  */
 public class ToolSwitching {
 
+	private Map<Integer,Set<Integer>> schedule = null;
+	
+	private static String DIR = "matrices"+ File.separator;
+	
 	private ToolSwitching() {
 		
 		System.out.println("Started algorithm...");
+		
+		schedule = InstanceImporter.importTspFile(DIR+"matrix_10j_10to_NSS_0.txt");
 		
 	}
 
